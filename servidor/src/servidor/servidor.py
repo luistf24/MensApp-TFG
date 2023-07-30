695f0693e3c61d7bad67696045dc58fed9ff276a
luistf24 <luis.tormo24@gmail.com>
2023-07-30 12:22:46 +0200

    fix: Corrijo algunos fallos #14
    Faltaba hacer un bucle infinito en la función escuchar_clientes.

--- servidor/src/servidor/servidor.py
+++ servidor/src/servidor/servidor.py
@@ -13,37 +13,39 @@ socket_servidor.bind((SERVER_IP, SERVER_PORT))
 socket_servidor.listen(5)
 
 def escuchar_clientes(sc):
-    try:
-        mensaje = sc.recv(1024).decode()
-    
-    except Exception as error:
-        print("[!] Error: {error}")
+    while True:
+        try:
+            mensaje_completo = sc.recv(1024).decode()
 
-    else:
-        emisor   = mensaje.split(separador, 2)[0]
-        receptor = mensaje.split(separador, 2)[1] 
-        mensaje  = mensaje.split(separador, 2)[2]
-
-        if(receptor == "servidor"): # Almacenamos el id del usuario, este caso solo se da cuando se regis-
-                                    # tra un usuario con un id que se almacenará en el mensaje sin cifrar.
-           sockets_usuarios[sc] = mensaje 
+        except Exception as error:
+            print("[!] Error: {}".format(error))
 
         else:
-            temp = False
-            for usuario in sockets_usuarios.keys():
-                if(receptor == sockets_usuarios[usuario]):
-                    usuario.send(msg.encode())
-                    temp = True
+            emisor   = mensaje_completo.split(separador, 2)[0]
+            receptor = mensaje_completo.split(separador, 2)[1] 
+            mensaje  = mensaje_completo.split(separador, 2)[2]
 
-            if(temp == False):
-                no_enviado = "servidor" + separador + emisor + separador + noreceptor + receptor
-                sc.send(no_enviado.encode())
+            if(receptor == "servidor"): # Almacenamos el id del usuario, este caso solo se da cuando se regis-
+                                    # tra un usuario con un id que se almacenará en el mensaje sin cifrar.
+                sockets_usuarios[sc] = mensaje 
+
+            else:
+                temp = False
+                for socket_usuario, usuario_id in sockets_usuarios.items():
+
+                    if(receptor == usuario_id): 
+                        socket_usuario.send(mensaje_completo.encode())
+                        temp = True
+
+                    # if(temp == False):
+                        # no_enviado = "servidor" + separador + emisor + separador + noreceptor + receptor
+                        # sc.send(no_enviado.encode())
 
 
 while True:
     socket_cliente, direccion_cliente = socket_servidor.accept()
 
-    print("[+] {direccion_cliente} se ha conectado.")
+    print("[+] {} se ha conectado.".format(direccion_cliente))
 
     sockets_usuarios[socket_cliente] = "UsuarioTemporal"
 
