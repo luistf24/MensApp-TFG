459fa2e8ff62945a87aaabff514c1fceed3125cb
luistf24 <luis.tormo24@gmail.com>
2023-07-31 19:22:10 +0200

    feat: Termino los test #21
    Testeo las funciones encriptar y desencriptar.

--- cliente/tests/test_cliente.py
+++ cliente/tests/test_cliente.py
@@ -5,3 +5,23 @@ def test_port():
 
 def test_ip():
     assert SERVER_IP == "127.0.0.1"
+
+def test_rellenar():
+    a = "prueba"
+    b = rellenar_bloque(a)
+
+    assert len(b) == 16
+
+def test_vaciar():
+    a = rellenar_bloque("prueba")
+    b = vaciar_bloque(a) 
+
+    assert len(a) != len(b)
+
+def test_cifrar_descifrar():
+    mensaje = "esto es una prueba"
+
+    mensaje_cifrado, tag, nonce = encriptar(mensaje, "prueba")
+    mensaje_descifrado = desencriptar(mensaje_cifrado, "prueba", tag, nonce)
+
+    assert mensaje == mensaje_descifrado.decode('utf-8')
